46e8afad0f46a288cbe5f73a005b4176419136bc
luanfgoncalves <luanfgoncalves@outlook.com>
2022-05-25 19:02:56 -0300

    exercicio 3 - getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const filterCondition = (element) => element.lastName === employeeName;
+  return data.employees.filter(filterCondition);
 }
 
 module.exports = getEmployeeByName;
+
+// --- testes ---
+
+console.log(getEmployeeByName());
+console.log(getEmployeeByName('Nelson'));
+console.log(getEmployeeByName('inexistente'));
+
+// --- Organização e Lógica ---
+
+// 1- adiciona um if (!employeeName) pra que o retorno seja '{}'.
+// 2- adiciona uma condição para ser usada num filtro, que checa se 'data.employees.lastName' corresponde ao parâmetro passado.
+// 3- retorna o array filtrado com a condição pré definida.
