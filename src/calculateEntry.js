0698fb02ae3546e0b39e4f4b3d1eb2dde3700c53
luanfgoncalves <luanfgoncalves@outlook.com>
2022-05-30 02:23:53 -0300

    exercioco 9 - calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
   const childFilterCondition = (element) => element.age < 18;
   const childEntrants = entrants.filter(childFilterCondition);
   const adultFilterCondition = (element) => element.age < 18;
@@ -16,7 +15,11 @@ function countEntrants(entrants) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
+  const entrantInfo = countEntrants(entrants);
+  return (entrantInfo.child * 20.99) + (entrantInfo.adult * 49.99) + (entrantInfo.senior * 24.99);
 }
 
 module.exports = { calculateEntry, countEntrants };
@@ -25,3 +28,7 @@ module.exports = { calculateEntry, countEntrants };
 // 1 - implementa um filtro para cada tipo de entrant
 // 2 - retorn um array com keys para cada tipo de entrant
 // 3- atribui o valor ás keys conforme o complimento do arrays filtrados.
+
+// 4 - adiciona retorno 0, caso calculate entry não receba parâmetros ou receba um array vazio.
+// 5 - adiciona uma constante que recupera o numero de cada tipo de entrant através de countEntrants
+// 6 - retorna o valor final através de uma equação que multiplica o numero de cada entrant pelo valor contido em zoo_data
