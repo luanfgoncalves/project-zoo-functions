ef0af65d163b694eab8a8b474be3453a33645f82
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-01 11:37:23 -0300

    quesito 12 - getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeFindCondition = (element) => element.id === id;
+  const selectedEmployee = data.employees.find(employeeFindCondition);
+  const managedAnimal = selectedEmployee.responsibleFor[0];
+  const specieFindCondition = (element) => managedAnimal.includes(element.id);
+  const filteredSpecie = data.species.find(specieFindCondition);
+  const mapcondition = (element) => element.age;
+  const mapedAnimalData = filteredSpecie.residents.map(mapcondition);
+  let maxAge = mapedAnimalData[0];
+  mapedAnimalData.forEach((element) => {
+    if (element > maxAge) {
+      maxAge = element;
+    }
+  });
+  const Answer = filteredSpecie.residents.find((element) => element.age === maxAge);
+  return [Answer.name, Answer.sex, Answer.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+// --- testes ---
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+
+// --- organização e lógica ---
+// 1- cria condição e filtro para encontrar o funcionário por id.
+// 2- identifica a primeira especie manejada pelo funcionario, já que estas estão dispostas conforme a ordem delas em zoo data.
+// 3- mapeia as idades dos animais da especie manejada, retornando um array com as respectivas idades.
+// 4- identifica a maior idade através do forEach e atribui ela a uma variavel.
+// 5- Encontra o animal mais velho a partir da idade em maxAge, e atribui o objeto do animal mais velho a uma variavel.
+// 6- retorna uma array com tres propriedades que acessam as chaves contidas no objeto Answer, basicamente o array com as informações do bicho.
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
+const completeSchedule = {
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  },
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  },
+  Thursday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
+  },
+  Friday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
+  },
+  Saturday: {
+    officeHour: 'Open from 8am until 10pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'snakes', 'elephants'],
+  },
+  Sunday: {
+    officeHour: 'Open from 8am until 8pm',
+    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
+  },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!scheduleTarget || scheduleTarget === null || scheduleTarget === undefined) {
+    return completeSchedule;
+  }
+  const findCondition = (element) => scheduleTarget.includes(element.name);
+  const foundAnimals = data.species.find(findCondition);
+  return foundAnimals.availability;
 }
 
 module.exports = getSchedule;
+
+// --- Lógica e organização ---
+// 1- adiciona uma condição de busca para foundAnimals em que o array de disponibilidade é retornado conforme o nome do animal
+// 2- adiciona um if para checar se o parâmetro é adequado, e caso não seja retorna um Objeto contendo os horários completos do zoologico.
