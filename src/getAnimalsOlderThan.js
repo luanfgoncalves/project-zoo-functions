6dfc8c92098d4baed1016d0c906805ff3ab93fb8
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-01 19:34:40 -0300

    teste

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,9 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const filterCondition = (element) => element.name === animal;
-  const selectedAnimal = data.species.filter(filterCondition);
-  const ageCondition = (element) => element.residents.age > age;
-  return selectedAnimal.every(ageCondition);
+  const selectedAnimal = data.species.find(filterCondition);
+  const ageCondition = (element) => element.age >= age;
+  return selectedAnimal.residents.every(ageCondition);
 }
 
 module.exports = getAnimalsOlderThan;
@@ -16,10 +16,14 @@ console.log('---');
 console.log(getAnimalsOlderThan('lions', 5));
 console.log('---');
 console.log(getAnimalsOlderThan('tigers', 18));
+console.log('---');
+console.log(getAnimalsOlderThan('otters', 7));
+console.log('---');
+console.log(getAnimalsOlderThan('penguins', 10));
 
 // --- Organização e Lógica ---
 
 // 1- adicionar uma condição para filtar os animais por especie usando o nome.
 // 2- gera um novo array(selectedAnimal) contendo os dados do animal selecionado atráves do filter.
-// 3- criar uma condição que verifica se a idade dos animais(selectedAnimal.residents.age) é '>' o parâmetro age.
+// 3- criar uma condição que verifica se a idade dos animais(selectedAnimal.residents.age) é '>=' o parâmetro age.
 // 4- usa Array.every para averiguar se a condição é verdadeira para todos.
